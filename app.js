09f289351cfaaabae97acb20079ed6ef15afc05b
buoyantair <buoyantair@protonmail.com>
2019-04-11 11:51:12 +0530

    Implement basic drag and drop

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const imagesList = document.querySelector("#images-list");
 const uploadField = document.querySelector("#upload-field");
 const submitButton = document.querySelector("#submit");
+const addTextButton = document.querySelector("#addText");
+const canvasElement = document.querySelector("#dropzone");
+
+const canvasElements = [];
 
 async function fetchImages() {
   const imageURLsResponse = await fetch("/images");
@@ -9,9 +13,11 @@ async function fetchImages() {
 
 function generateImage(src, parentNode) {
   const imageContainer = document.createElement("li");
+  const uniqueID = src.match(/uploads\-\d+/i)[0];
   const image = document.createElement("img");
   image.setAttribute("src", src);
-
+  image.setAttribute("id", uniqueID);
+  image.addEventListener("dragstart", dragStartHandler);
   imageContainer.appendChild(image);
   parentNode.appendChild(imageContainer);
 }
@@ -64,8 +70,69 @@ async function uploadImage() {
   }
 }
 
+function dragStartHandler(ev) {
+  // Add the target element's id to the data transfer object
+  ev.dataTransfer.setData("text/plain", ev.target.id);
+  ev.dataTransfer.dropEffect = "move";
+}
+
+function dragOverHandler(ev) {
+  ev.preventDefault();
+  // Set the dropEffect to copy
+  ev.dataTransfer.dropEffect = "copy";
+}
+
+function dropHandler(e) {
+  e.preventDefault();
+  const { layerX: x, layerY: y } = e;
+  // Get the id of the target and add the moved element to the target's DOM
+  var data = e.dataTransfer.getData("text/plain");
+  if (data) {
+    const clone = document.getElementById(data).cloneNode(true);
+    clone.removeAttribute("draggable");
+    canvasElement.appendChild(clone);
+    moveElement(clone, x, y)
+  }
+}
+
+let currentDragElm = null;
+
+function moveElement(element, x, y) {
+  if (element) {
+    const { width, height } = element.getBoundingClientRect();
+    element.style.transform = `translate(${x - width / 2}px, ${y -
+      height / 2}px)`;
+  }
+}
+
+function dragHandler(e) {
+  e.preventDefault();
+  const { layerX: x, layerY: y } = e;
+  if (currentDragElm !== null && currentDragElm === e.target) {
+    moveElement(currentDragElm, x, y);
+  }
+}
+
+function mouseDownHandler(e) {
+  e.preventDefault();
+  const isChild = canvasElement.contains(e.target) && e.target !== canvasElement;
+  if (isChild) {
+    currentDragElm = e.target;
+    canvasElement.addEventListener("mousemove", dragHandler);
+  }
+}
+
+function mouseUpHandler(e) {
+  e.preventDefault();
+  canvasElement.removeEventListener("mousemove", dragHandler);
+}
+
 fetchImages()
   .then(memoizeImageList)
   .then(generateImageList);
 
 submitButton.addEventListener("click", uploadImage);
+canvasElement.addEventListener("drop", dropHandler);
+canvasElement.addEventListener("dragover", dragOverHandler);
+canvasElement.addEventListener("mousedown", mouseDownHandler);
+canvasElement.addEventListener("mouseup", mouseUpHandler);
